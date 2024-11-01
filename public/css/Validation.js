42f81f6c7288e1802c47364a4d0055b3efc1e8b0
Athisakthiram-dev <athispeaks@gmail.com>
2024-11-01 07:41:43 +0530

    Update Validation.js

--- public/css/Validation.js
+++ public/css/Validation.js
@@ -1,1 +1,21 @@
 
+document.addEventListener('DOMContentLoaded', function(){
+    const appointmentForm=document.getElementById('appointmentForm');
+    appointmentForm.addEventListener('submit',function (event){
+        const phonenumber=document.getElementById('phonenumber').value;
+        const appointmentDate=document.getElementById('appointment_date').value;
+        const today=new Date();
+
+        //phonenumber
+        if(phonenumber.length !==10 || isNaN(phonenumber)){
+            alert("Please enter the valid phonenumber!");
+            event.preventDefault();
+            return;
+        }
+        if(appointmentDate < today.setHours(0,0,0,0)){
+            alert("Event cannot be in past");
+            event.preventDefault();
+            return;
+        }
+    })
+})
